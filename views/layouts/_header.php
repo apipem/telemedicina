0681fa5e8a7e4c210026ea41fdee793510929ae2
apipem <andres.mayorga15@gmail.com>
2024-09-18 22:50:06 -0500

    menu

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -1,10 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Telemedicina</a>
+        <button class="btn btn-secondary" onclick="history.back()">Volver</button>
+
+        <div class="mx-auto text-center" style="flex-grow: 1;">
+            <a class="navbar-brand" style="text-align: center;" href="#">Telemedicina</a>
+        </div>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
