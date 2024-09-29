b1dcd6a50e7522c5662f94bf3861293053c98ce3
apipem <38577543+apipem@users.noreply.github.com>
2024-09-29 18:14:37 -0500

    Update _footer.php

--- views/layouts/_footer.php
+++ views/layouts/_footer.php
@@ -2,11 +2,9 @@
 <footer class="main-footer">
     <div class="container">
         <div class="row">
-            <!-- To the right -->
-            <div class="col-md12 text-md-end">
-                <p> Copyright &copy; 2024 Telemedicina @ - Todos los derechos reservados</p>
+            <div class="col-12 text-md-end text-center">
+                <p>Copyright &copy; 2024 Telemedicina - Todos los derechos reservados</p>
             </div>
-            <!-- Default to the left -->
         </div>
     </div>
 </footer>
