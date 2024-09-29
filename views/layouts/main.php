457a6507ee3a3fa86b71d0e6d3c3dba588d6ea62
apipem <38577543+apipem@users.noreply.github.com>
2024-09-29 18:14:29 -0500

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,7 +14,6 @@ if ($session->isActive && isset(Yii::$app->user->identity->id)) {
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <!-- Otros elementos head -->
         <?php
         $randomIconUrl = 'https://favicongrabber.com/api/grab/' . urlencode('https://example.com'); // Cambia 'https://example.com' por la URL del sitio del que deseas obtener el favicon
         ?>
@@ -31,22 +30,22 @@ if ($session->isActive && isset(Yii::$app->user->identity->id)) {
         <!-- Theme style -->
         <link rel="stylesheet" href="<?= Yii::$app->getUrlManager()->createUrl('css/adminlte.min.css') ?>">
         <!-- Bootstrap CSS -->
-        <!-- Importar Bootstrap 5 CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Custom CSS -->
         <link rel="stylesheet" href="<?= Yii::$app->getUrlManager()->createUrl('css/main.css') ?>">
+
         <style>
             .bg-image {
-                background-image: url('<?= \yii\helpers\Url::to('@web/images/banner-medico-medico-gafas.jpg') ?>'); /* Ruta de la imagen */
-                background-size: cover; /* Asegura que la imagen cubra todo el contenedor */
-                background-position: center; /* Centra la imagen */
-                position: relative; /* Necesario para el overlay */
-                min-height: 100vh; /* Ajusta la altura según necesites */
-                color: white; /* Cambia el color del texto a blanco */
+                background-image: url('<?= \yii\helpers\Url::to('@web/images/banner-medico-medico-gafas.jpg') ?>');
+                background-size: cover;
+                background-position: center;
+                position: relative;
+                min-height: 100vh;
+                color: white;
             }
 
             .bg-image p {
-                color: white; /* Asegura que todos los textos en <p> sean blancos */
+                color: white;
             }
 
             .bg-image::before {
@@ -56,74 +55,74 @@ if ($session->isActive && isset(Yii::$app->user->identity->id)) {
                 left: 0;
                 right: 0;
                 bottom: 0;
-                background-color: rgba(0, 0, 0, 0.5); /* Fondo negro con 50% de opacidad */
-                z-index: 1; /* Asegura que esté debajo del contenido */
+                background-color: rgba(0, 0, 0, 0.5);
+                z-index: 1;
             }
 
             .row {
-                position: relative; /* Para asegurar que el contenido esté encima del overlay */
-                z-index: 2; /* Asegura que el contenido se vea por encima del overlay */
+                position: relative;
+                z-index: 2;
             }
         </style>
     </head>
 
-    <body class="">
-    <?php echo $this->render("_sidebar-left") ?>
-    <div class="wrapper">
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <?php echo $this->render("_header") ?>
-                </div><!-- /.container-fluid -->
-            </div><!-- /.content-header -->
+    <body>
+        <?php echo $this->render("_sidebar-left") ?>
+        <div class="wrapper">
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <?php echo $this->render("_header") ?>
+                    </div><!-- /.container-fluid -->
+                </div><!-- /.content-header -->
 
-            <?php $this->beginBody(); ?>
+                <?php $this->beginBody(); ?>
 
-            <!-- Main content -->
-            <div class="container bg-image">
-                <div class="row">
-                    <div class="col">
-                        <?php echo $content ?>
+                <!-- Main content -->
+                <div class="container bg-image">
+                    <div class="row">
+                        <div class="col">
+                            <?php echo $content ?>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <?php $this->endBody(); ?>
-        </div><!-- /.content-wrapper -->
-    </div><!-- ./wrapper -->
+                <?php $this->endBody(); ?>
+            </div><!-- /.content-wrapper -->
+        </div><!-- ./wrapper -->
 
-    <?= $this->render("_footer"); ?>
+        <?= $this->render("_footer"); ?>
 
-    <!-- REQUIRED SCRIPTS -->
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="<?= Yii::$app->getUrlManager()->createUrl('js/bootstrap.bundle.min.js') ?>"></script>
-    <!-- AdminLTE App -->
-    <script src="<?= Yii::$app->getUrlManager()->createUrl('js/adminlte.min.js') ?>"></script>
-    <!-- Custom Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <!-- Importar Bootstrap 5 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-    <script>
-        $(document).ready(function() {
-            var currentUrl = window.location.pathname;
+        <!-- REQUIRED SCRIPTS -->
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="<?= Yii::$app->getUrlManager()->createUrl('js/bootstrap.bundle.min.js') ?>"></script>
+        <!-- AdminLTE App -->
+        <script src="<?= Yii::$app->getUrlManager()->createUrl('js/adminlte.min.js') ?>"></script>
+        <!-- Custom Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <!-- Importar Bootstrap 5 JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
-            $(".nav-link").each(function() {
-                var linkUrl = $(this).attr("href");
+        <script>
+            $(document).ready(function() {
+                var currentUrl = window.location.pathname;
 
-                if (currentUrl === linkUrl || currentUrl.startsWith(linkUrl + '/')) {
-                    $(this).addClass("active");
-                    $(this).closest(".nav-item").addClass("menu-open");
-                }
+                $(".nav-link").each(function() {
+                    var linkUrl = $(this).attr("href");
+
+                    if (currentUrl === linkUrl || currentUrl.startsWith(linkUrl + '/')) {
+                        $(this).addClass("active");
+                        $(this).closest(".nav-item").addClass("menu-open");
+                    }
+                });
             });
-        });
+        </script>
 
-
-    </script>
+    </body>
     </html>
 
     <?php
@@ -134,4 +133,9 @@ if ($session->isActive && isset(Yii::$app->user->identity->id)) {
 
     <?php $this->beginBody(); ?>
     <?php echo $this->render("index"); ?>
-    <?php $this->endPage(); } ?>
+    <?php $this->endBody(); ?>
+
+    <?php
+    $this->endPage();
+}
+?>
